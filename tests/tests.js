9f533fc8a092c745ceac6a7c51357a5139a37941
aleksei <aleksei@netbook.vyadre>
2015-03-06 00:53:03 +0400

    Добавлен метод async.forEach(arrArg, func, callback), переработан метод async.map(arrArg, func, callback), добавлены тесты.

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,8 @@
 var async = require('../lib/vyadre.async.js');
-var testsF, testsRandom, testsRandomF;
+var tests, testsF, testsRandom, testsRandomF;
 
 
-var perfTestCount = 1000;
+var perfTestCount = 10000000;
 
 async.series([function(next){
   var st = Date.now();
@@ -27,7 +27,7 @@ async.series([function(next){
     next();
   });
 }, function(next){
-  var tests = [];
+  tests = [];
   for(var i = 0; i < perfTestCount; i++){
     tests.push(i);
   }
@@ -52,6 +52,31 @@ async.series([function(next){
     next();
   });
 }, function(next){
+  var tsWaterfall = Date.now();
+  async.waterfall(testsF, function(){
+    var tWaterfall = Date.now() - tsWaterfall;
+    console.log("Sped test time async.waterfall:", tWaterfall);
+    next();
+  });
+}, function(next){
+  var tsForEach = Date.now();
+  async.forEach(tests, function(i, next){
+    next();    
+  }, function(){
+    var tForEach = Date.now() - tsForEach;
+    console.log("Sped test time async.forEach:", tForEach);
+    next();
+  });
+}, function(next){
+  var tsMap = Date.now();
+  async.map(tests, function(i, next){
+    next(0);    
+  }, function(){
+    var tMap = Date.now() - tsMap;
+    console.log("Sped test time async.map:", tMap);
+    next();
+  });
+}, function(next){
   testsRandom = [];
   for(var i = 0; i < 100; i++){
     testsRandom.push(i);
@@ -59,7 +84,7 @@ async.series([function(next){
   testsRandomF = testsRandom.map(function(i){
     return function(next){
       setTimeout(function(){
-	console.log(i);
+//	console.log(i);
 	next(i === 15 ? i : null);
       }, Math.random() * 10);
     };
@@ -80,17 +105,29 @@ async.series([function(next){
 }, function(next){
   console.log("testsRandomF async.map");
   async.map(testsRandom, function(i, next){
-    console.log("async.map:", i);
+//    console.log("async.map:", i);
     next();
+  }, next)
+}, function(next){
+  console.log("testsRandomF async.forEach");
+  async.forEach(testsRandom, function(i, next){
+    setTimeout(function(){
+//      console.log("async.forEach:", i);
+      next();
+    }, Math.random() * 10);
   }, next)
 }, function(next){
   console.log("testsRandomF async.map");
   async.map(testsRandom, function(i, next){
     setTimeout(function(){
-      console.log("async.map:", i);
-      next();
+//      console.log("async.map:", i);
+      next(i * 2);
     }, Math.random() * 10);
-  }, next)
+  }, function(arrRes){
+    console.log("arrRes:", arrRes);
+    next(); 
+  });
+  //}, next)
 }], function(){
   console.log("Все тесты завершены =)");
 });
